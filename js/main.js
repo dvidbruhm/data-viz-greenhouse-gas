b5ba85f1cc0aab86f214db520ed27c9434bfecd6
Saaaber <37632033+Saaaber@users.noreply.github.com>
2018-04-08 20:55:24 -0400

    Format stuff

--- js/main.js
+++ js/main.js
@@ -4,8 +4,7 @@ var globalData = undefined;
 
 (function (L, d3, topojson, searchBar, localization) {
 
-
-    init_filter_panel();
+	init_filter_panel();
 
     /***** Échelles utilisées *****/
     //TODO
@@ -24,12 +23,27 @@ var globalData = undefined;
             /***** Prétraitement des données *****/
             var data = cleandata(rawdata);
             globalData = data;
-
+			
+			var sumCO2 = 0;
+			var i;
+			var j;
+			
+			for (i=0; i<data.length; i++) {
+				for (j=0; j<data[i].years.length; j++) {
+					sumCO2 = sumCO2 + data[i].years[j].total_eq;
+				}
+			}
+			sumCO2 = sumCO2 / 1000000;
+			sumCO2.toFixed(2);
+			document.getElementById("listitem1").innerHTML = "Plus de " + sumCO2 + " mégatonnes de CO<sub class=\"sub-c\">2 eq</sub> ont été émises.";
+			document.getElementById("listitem2").innerHTML = data.length + " compagnies ont émis plus de 50 kilotonnes de CO<sub class=\"sub-c\">2 eq</sub> par année.";
+			
             main_vizgaz();
             main_barchart(data, localization);
             linechart(data);
 			radar(data);
-
+			
+			
             //TODO
             $(window).resize(function(){
                 setTimeout(function() {
@@ -38,7 +52,6 @@ var globalData = undefined;
             });
         });
 
-
-
+	
 
 })(L, d3, topojson, searchBar, localization);
